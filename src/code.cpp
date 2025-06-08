14b2685d4250507ae536b1096049db007d58c23b
wuinspatan <kiritoxoly@gmail.com>
2025-06-08 18:06:37 +0700

    New with set ip

--- src/code.cpp
+++ src/code.cpp
@@ -3,6 +3,7 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiServer.h>
+#include <time.h>
 
 #define DHT_SENSOR_PIN  21 // ESP32 pin GPIO21 connected to DHT22 sensor
 #define DHT_SENSOR_TYPE DHT22
@@ -11,30 +12,38 @@ WiFiServer server(23) ;
 WiFiClient client ;
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
-const char* ssid = "YOUR_SSID_WIFI" ;         // Input Your Wi-Fi Name
-const char* wifi_password = "YOUR_PW_WIFI" ;    // Input Your Wi-Fi Password
+const char* ssid = "xx" ;         // Input Your Wi-Fi Name
+const char* wifi_password = "xx" ;    // Input Your Wi-Fi Password
 
 void connectToWIFI() {
-  delay(1000) ;
-  WiFi.mode(WIFI_STA) ;
-  WiFi.begin(ssid, wifi_password) ;
-  Serial.println("\nConnecting") ;
+  delay(1000);
+  WiFi.mode(WIFI_STA);
 
-  unsigned long startAttempTime = millis() ;
+  // Set static IP configuration
+  IPAddress local_IP(192, 168, 68, xx); // set your ip address
+  IPAddress gateway(192, 168, 68, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress dns(8, 8, 8, 8);
 
+  WiFi.config(local_IP, gateway, subnet, dns);
+
+  WiFi.begin(ssid, wifi_password); // your actual Wi-Fi password here
+  Serial.println("\nConnecting");
+
+  unsigned long startAttempTime = millis();
   while (WiFi.status() != WL_CONNECTED && millis() - startAttempTime < 10000) {
-    Serial.print(".") ;
-    delay(500) ;
+    Serial.print(".");
+    delay(500);
   }
 
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("\nFailed to connect to WIFI.") ;
+    Serial.println("\nFailed to connect to WIFI.");
   } else {
-    Serial.println("\nConnected to the WiFi network") ;
-    Serial.print("Local ESP32 IP: ") ;
-    Serial.println(WiFi.localIP()) ;
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
   }
-}//end connectTowifi 
+} 
 
 void connectToServer() {
   server.begin() ;
@@ -89,5 +98,7 @@ void loop() {
 }//end loop
 
 
-// Local ESP32 IP: 192.168.68.77 (ESP32 devkit V1)
-// My local IP: 127.0.0.1 (win@NixOS)
+// Local ESP32 IP: 192.168.68.52 (ESP32 devkit V1)
+// pio run -t upload 
+// pio device monitor
+// click EN botton
